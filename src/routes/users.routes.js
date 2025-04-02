ba131adc67c8b299f5f799d3636c908d79753e4d
vinsmake <ps.enrique.e@gmail.com>
2025-04-02 15:05:32 -0600

    get & post added

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,8 +2,31 @@ import { Router } from "express";
 
 const router = Router();
 
+/* Routes */
+/* Get users */
 router.get("/users", (req, res) => {
-  res.send("Getting users");
+    res.send("Getting users");
 });
+
+/* Get users by id */
+router.get("/users/:UserId", (req, res) => {
+    const { UserId } = req.params;
+    res.send("Getting user by id: " + UserId);
+});
+
+/*  */
+router.post("/users", (req, res) => {
+    res.send("Creating user");
+});
+
+/*  */
+router.delete("/users/:UserId", (req, res) => {
+    res.send("Deleting user by id: " + req.params.UserId);
+});
+
+
+
+
+
 
 export default router;
