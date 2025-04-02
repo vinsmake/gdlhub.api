c10ebe41da9a80685bdc8ea8bad3cabf97bae0ac
vinsmake <ps.enrique.e@gmail.com>
2025-04-02 12:43:41 -0600

    router added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 
 import express from "express";
 import { PORT } from "./config.js";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
+
+app.use(userRoutes);
 
 app.listen(PORT);
 console.log(`Server is running on http://localhost:${PORT}`);
